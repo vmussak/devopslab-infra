e9cf2ce99634c0659ef24a21a7c49d9abffa584b
Vinicius Mussak <vinicius.mussak@outlook.com>
2022-03-17 20:32:55 -0300

    Variavel de ambiente terraform cloud

--- providers.tf
+++ providers.tf
@@ -9,9 +9,6 @@ terraform {
 
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
-
-  credentials = file("/home/vagrant/gcpkey.json")
-
   project = "lab-devops-cloud-344223"
   region  = "us-west1"
   zone    = "us-west1-b"
